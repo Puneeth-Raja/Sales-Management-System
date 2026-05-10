2bc3ce0d3bacef0810ce507412df8275f57a6d0e
Puneeth <kpuneethraja@gmail.com>
2026-05-11 00:40:17 +0530

    Updated secure database configuration

--- db.py
+++ db.py
@@ -1,10 +1,23 @@
-import mysql.connector
+# import mysql.connector
 
-def get_connection ():
-    return mysql.connector.connect( 
-        host='localhost',
-        user='root',
-        password='Puneeth@16',
-        database= 'sales_intelligence_hub'
-    )
+# def get_connection ():
+#     return mysql.connector.connect( 
+#         host='localhost',
+#         user='root',
+#         password='Puneeth@16',
+#         database= 'sales_intelligence_hub'
+#     )
        
+import mysql.connector
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def get_connection():
+    return mysql.connector.connect(
+        host=os.getenv("db_host"),
+        user=os.getenv("db_user"),
+        password=os.getenv("db_password"),
+        database=os.getenv("db_database")
+    )
