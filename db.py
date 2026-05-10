44160662d953edf839a841e8ce7a2afd9534fa75
Puneeth <kpuneethraja@gmail.com>
2026-05-11 00:58:04 +0530

    latest version

--- db.py
+++ db.py
@@ -1,13 +1,3 @@
-# import mysql.connector
-
-# def get_connection ():
-#     return mysql.connector.connect( 
-#         host='localhost',
-#         user='root',
-#         password='Puneeth@16',
-#         database= 'sales_intelligence_hub'
-#     )
-       
 import mysql.connector
 import os
 from dotenv import load_dotenv
@@ -16,8 +6,8 @@ load_dotenv()
 
 def get_connection():
     return mysql.connector.connect(
-        host=os.getenv("db_host"),
-        user=os.getenv("db_user"),
-        password=os.getenv("db_password"),
-        database=os.getenv("db_database")
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
